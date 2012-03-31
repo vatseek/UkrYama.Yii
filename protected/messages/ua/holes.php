c113d351953878c928106f2e52cd3970cef525fe
smart <smart@smart-HP-ubuntu.(none)>
2012-03-31 20:53:02 +0300

    Widget

---
+++ protected/messages/ua/holes.php
@@ -0,0 +1,10 @@
+<?php
+$MESS['WIDGET_DEFAULT_REGION']         = 'Область';
+$MESS['WIDGET_DEFAULT_CITY']           = 'Місто';
+$MESS['WIDGET_TYPE_DEFECT']           = 'Тип дефекту';
+$MESS['WIDGET_STATUS_DEFECT']           = 'Статус дефекту';
+$MESS['WIDGET_SUBMIT_DEFECT']           = 'Показати';
+$MESS['WIDGET_CLEAR_DEFECT']           = 'Очистити';
+
+return $MESS;
+?>
