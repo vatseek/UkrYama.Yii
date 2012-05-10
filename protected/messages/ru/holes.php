a1afc9275d0a7f62a24d518bc7fa895484b22821
belousoff <i@belousoff.com>
2012-05-10 23:52:04 +0300

    Заменено «ГИБДД» на «ГАИ»

--- protected/messages/ru/holes.php
+++ protected/messages/ru/holes.php
@@ -1,9 +1,9 @@
 <?php
 $MESS['WIDGET_DEFAULT_REGION']         = 'Область';
 $MESS['WIDGET_DEFAULT_CITY']           = 'Город';
-$MESS['WIDGET_TYPE_DEFECT']           = 'Тип дефекта';
-$MESS['WIDGET_STATUS_DEFECT']           = 'Статус дефекта';
-$MESS['WIDGET_SUBMIT_DEFECT']           = 'Показать';
+$MESS['WIDGET_TYPE_DEFECT']            = 'Тип дефекта';
+$MESS['WIDGET_STATUS_DEFECT']          = 'Статус дефекта';
+$MESS['WIDGET_SUBMIT_DEFECT']          = 'Показать';
 $MESS['WIDGET_CLEAR_DEFECT']           = 'Сбросить';
 
 return $MESS;
