7e879043017cccb00907d1f1999e1b86db15fc49
belousoff <i@belousoff.com>
2012-05-11 00:01:15 +0300

    fix lang

--- protected/messages/ua/errors.php
+++ protected/messages/ua/errors.php
@@ -1,6 +1,6 @@
 <?php
 return Array(
-			'GREENSIGHT_ERROR_NOID'=>'Элемент не найден!!!!',
+			'GREENSIGHT_ERROR_NOID'=>'Элемент не найден',
 			'GREENSIGHT_ERROR_UNSUPPORTED_IMAGE_TYPE'=>'Неподдерживаемый формат изображения',
 			'GREENSIGHT_ERROR_DATABASE'=>'Ошибка базы данных :(',
 			'GREENSIGHT_ERROR_CANNOT_CREATE_DIR'=>'Ошибка при загрузке файла',
