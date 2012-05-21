8cdf16cfd1f697daa8214aca957abad83ccca1b3
sck_v <ivan_kryak@mail.ru>
2012-05-21 23:00:32 +0600

    #1 Regulations page translation

--- protected/views/site/pages/regulations.php
+++ protected/views/site/pages/regulations.php
@@ -3,29 +3,29 @@ $this->pageTitle=Yii::app()->name . ' - Стандарты и нормативы
 $this->title='Стандарты и нормативы';
 $this->layout='//layouts/header_blank';
 ?>
-<h2>Государственный стандарт Украины 3587-97 — «Автомобильные дороги, улицы и железнодорожные переезды. Требования к эксплуатационному состоянию»&raquo;</h2>
-	<p>Настоящий стандарт устанавливает, что покрытие проезжей части не должно иметь просадок, выбоин, наплывов или иных деформаций, затрудняющих движение транспортных средств, а состояние покрытия проезжей части и других элементов дороги должны проверяться организациями, которые их эксплуатируют, — каждые 10 дней. Всё это должно обеспечивать безопасность движения транспортных средств и пешеходов.</p>
+<h2><?php echo Yii::t("static", "STATIC_REGULATIONS_HEADER");?></h2>
+	<p><?php echo Yii::t("static", "STATIC_REGULATIONS_P_1");?></p>
  	
-  <p>Повреждениями дорожного покрытия являются:
+  <p><?php echo Yii::t("static", "STATIC_REGULATIONS_P_2");?>
     <br />
    		</p>
 
   <ul class="default"> 			
-    <li>Яма — 0,5 м&sup2; (весной и осенью — 3 м²) на 100 000 м²;</li>
+    <li><?php echo Yii::t("static", "STATIC_REGULATIONS_UL_LI_1");?></li>
    			
-    <li>Глубина просадки или выбоины — от 6 см;</li>
+    <li><?php echo Yii::t("static", "STATIC_REGULATIONS_UL_LI_2");?></li>
    			
-    <li>Высота наплыва — от 4 см;</li>
+    <li><?php echo Yii::t("static", "STATIC_REGULATIONS_UL_LI_3");?></li>
    			
-    <li>Отклонение высотной отметки крышки люка относительно уровня покрытия проезжей части — 1 см, а дождеприемного колодца — 2 см;</li>
+    <li><?php echo Yii::t("static", "STATIC_REGULATIONS_UL_LI_4");?></li>
    			
-    <li>Отклонение верха головок рельсов трамвайных или железнодорожных путей, расположенных в пределах проезжей части, относительно поверхности покрытия — 2 см.</li>
+    <li><?php echo Yii::t("static", "STATIC_REGULATIONS_UL_LI_5");?></li>
    		</ul>
  	
   <p></p>
  	
-  <p>Срок устранения повреждения — 1—10 суток с момента обнаружения.</p>
+  <p><?php echo Yii::t("static", "STATIC_REGULATIONS_P_3");?></p>
  	
-  <p><a href="/files/DSTU3587-97UA.pdf" target="_blank" ><img src="/images/pdf_doc.gif" height="14" width="14"  /></a> <a href="/files/DSTU3587-97UA.pdf" target="_blank" >Прочитать или скачать Государственный стандарт Украины 3587-97</a> (.pdf, 380 Кб)</p>
+  <p><a href="/files/DSTU3587-97UA.pdf" target="_blank" ><img src="/images/pdf_doc.gif" height="14" width="14"  /></a> <a href="/files/DSTU3587-97UA.pdf" target="_blank" ><?php echo Yii::t("static", "STATIC_REGULATIONS_P_A_1");?></a> (.pdf, 380 Кб)</p>
  	
-  <p><a href="/files/DSTU3587-97UA.pdf" target="_blank" ><img src="/images/pdf_doc.gif" height="14" width="14"  /></a> <a href="/files/P-G.1-218-113-2009.pdf" target="_blank" >Прочитать или скачать технические правила ремонта и содержания автомобильных дорог общего пользования П-Г.1-218-113: 2009</a> (.pdf, 15 Мб)</p>
+  <p><a href="/files/DSTU3587-97UA.pdf" target="_blank" ><img src="/images/pdf_doc.gif" height="14" width="14"  /></a> <a href="/files/P-G.1-218-113-2009.pdf" target="_blank" ><?php echo Yii::t("static", "STATIC_REGULATIONS_P_A_2");?></a> (.pdf, 15 Мб)</p>
