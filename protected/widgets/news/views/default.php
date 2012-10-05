4811b72612faf86655e8b4e8f94713604eeba58c
Katrevich Sergey <skatrevich@gmail.com>
2012-10-05 12:01:44 +0300

    Фиксы от Олега

--- protected/widgets/news/views/default.php
+++ protected/widgets/news/views/default.php
@@ -1,3 +1,17 @@
+<div class="like">
+		<noindex><script type="text/javascript" src="//yandex.st/share/share.js" charset="utf-8"></script>
+<div class="yashare-auto-init" data-yashareL10n="ru" data-yashareType="button" data-yashareQuickServices="vkontakte,facebook,twitter,lj"></div></noindex><br />
+	<div id="banner_ap">
+		<div class="banner_yama">
+			<span class="">Партнер:</span><br />
+			<noindex><a href="http://autoportal.ua/" target="_blank" rel="nofollow"><img src="<?php echo Yii::app()->request->baseUrl;?>/banners/ap-bann-215x111.png" alt="" /></a></noindex>
+		</div>
+		<br />
+		<div id="partners">
+			SSL сертификаты и <a href="http://www.isplicense.ru/" target="_blank">лицензии ПО</a>
+		</div> 
+	</div>
+</div>
 <!-- NEWS -->
 <div class="news-list-mainpage">
 <?php foreach ($model as $news) : ?>	
